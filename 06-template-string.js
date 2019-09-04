775d4d66e55ba15d3d3c730d2b55ebd67d2ba778
zionHuang <hjszion@gmail.com>
2019-09-03 23:35:09 -0700

    added template-strings

--- 06-template-string.js
+++ 06-template-string.js
@@ -14,3 +14,37 @@ $('#result').append(`
    in your basket, <em>${basket.onSale}</em>
   are on sale!
 `);
+
+//模版字符串嵌套函数
+function fn() {
+    return "Hello World";
+  }
+  
+  `foo ${fn()} bar`
+  // foo Hello World bar
+
+//模版字符串嵌套模版字符串
+const tmpl = addrs => `
+  <table>
+  ${addrs.map(addr => `
+    <tr><td>${addr.first}</td></tr>
+    <tr><td>${addr.last}</td></tr>
+  `).join('')}
+  </table>
+`;
+//模版字符串嵌套模版使用方法
+const data = [
+    { first: '<Jane>', last: 'Bond' },
+    { first: 'Lars', last: '<Croft>' },
+];
+
+console.log(tmpl(data));
+// <table>
+//
+//   <tr><td><Jane></td></tr>
+//   <tr><td>Bond</td></tr>
+//
+//   <tr><td>Lars</td></tr>
+//   <tr><td><Croft></td></tr>
+//
+// </table>
