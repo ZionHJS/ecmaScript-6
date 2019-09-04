22fdecf0472d77cdb5f063071e47ec2f18991a84
zionHuang <hjszion@gmail.com>
2019-09-04 16:21:23 -0700

    updated object

--- 11-object.js
+++ 11-object.js
@@ -67,3 +67,71 @@ console.log(0 === (-0));
 console.log(NaN === NaN);
 
 Object.is(NaN,NaN); //比较两个参数
+
+//8. Object.assign方法用于对象的合并，将源对象(source)的所有可枚举属性 复制到目标对象(target) 这个是浅拷贝
+let m = {ai: 'aicoder'};
+let k = Object.assign(m, {a:1},{b:2},{c:3});
+console.log('k:',k);
+console.log('m:',m);
+
+//Object.assign 浅拷贝数字和布尔类型没有效果，字符串会转成字符数组
+let n = Object.assign({}, 33, true, {a:2});
+console.log('n:', n);
+
+let n = Object.assign({}, 'google.com');
+console.log('n:', n);
+
+//数组的assign
+let n = Object.assign({},['a', 2, 3, 'aaa']);
+console.log('n:', n);
+
+//同名属性的替换
+let m = {};
+let k = Object.assign(m, {age:18, name:''}, {age:20});
+console.log('k:', k);
+
+//给对象赋默认值
+//浅拷贝对象
+
+//9.属性的定义与描述 Object.defineProperties()
+let person = {};
+person.age = 10;
+
+console.log('person:', person);
+
+Object.defineProperties(person, {
+    name:{
+        configurable: true,
+        enumerable:true,
+        value:'google.com',
+        writable:true
+    },
+    phone:{
+        configurable:false,
+        enumerable:false,
+        writable:false,
+        value:'123123123'
+    },
+    address:{
+        get:function(){
+            return 'Beijing Newyork Shanghai'
+        },
+        set:function(){
+            return ''
+        }
+    }
+});
+console.log(person.phone);
+console.log('person:', person);
+console.log(person.address);
+
+for(let k in person){
+    console.log(k);
+}
+
+
+
+
+
+
+
