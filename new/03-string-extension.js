1c4b47bde7161076ecc932cc7ac6805c92cca34a
zionHuang <hjszion@gmail.com>
2019-09-06 00:36:26 -0700

    finishe string-extensions.js

--- new/03-string-extension.js
+++ new/03-string-extension.js
@@ -174,4 +174,80 @@ function fn(){
 }
 `foo ${fn()} bar`   //foo hello world bar
 
+//如果大括号内的值不是字符串 将按照一般规则转为字符串 比如 大括号中是一个对象 将默认调用对象的toString方法 
+//所以大括号中只能是字符串的存在
+
+//如果模版字符串中的变量没有声明 将报错
+let msg = 'hello, ${place}';
+
+//由于模版字符串的大括号内部 就是只从JavaScript代码 因此如果大括号内部是一个字符串 将会原样输出
+`hello ${world}`   //hello world
+
+//模版字符串甚至还能嵌套
+const tmpl = addr => `
+<table>
+${addr.map(addr => `
+<tr><td>${addr.first}</td></tr>
+<tr><td>${addr.last}</td></tr>`).join('')}
+</table>
+`;
+//上面代码中 模版字符串的变量之中 又嵌入了另一个模版字符串 使用方法如下
+const data = [ 
+    {first:'<jane>', last:'Bond'},
+    {first:'Lars', last:'<Croft>'},
+];
+console.log(tmpl(data));
+{/* <table>
+
+<tr><td><jane></td></tr>
+<tr><td>Bond</td></tr>
+<tr><td>Lars</td></tr>
+<tr><td><Croft></td></tr>
+</table> */}
+
+//标签模版 可以用来紧跟在一个函数名后面 该函数将被调用来处理这个模版字符串 这被称为 标签模版 功能(tagged template)
+alert`123`  //当作函数的参数来使用
+alert(123)
+//标签模版其实不是模版 而是函数调用的一种特殊形式 标签指的就是函数 紧跟在后面的模版字符串就是它的参数
+
+//如果模版字符串里面有变量 就不是简单的调用了, 而是会将模版字符串先处理成多个参数 再调用函数
+let a = 5; 
+let b = 10;
+
+tag`hello ${a+b} world ${a*b}`;
+//等同于
+tag(['hello','world ',''],15,50);
+
+//函数tag会依次接收到多个参数
+function tag(stringArr, value1, value2){
+    //...
+}
+//等同于
+function tag(stringArr, ...values){
+    //...
+}
+//tag函数的第一个参数是一个数组,数组的成员是那些没有变量替换的部分，
+//tag函数的其他参数 都是模版字符串各个变量被替换后的值 由于本例中 模版字符串含有两个变量 因此tag会接收到value1 和 value2两个参数
+//tag函数所有参数的实际值如下
+//第一个参数 ['hello','world','']
+//第二个参数 15
+//第三个参数 50
+//也就是说 tag函数实际上以下面的形式调用
+tag(['hello','world',''],15,50)
+
+//我们可以按照需要编写tag函数的代码 下面是tag函数的一种写法 以及运行结果
+let a = 5;
+let b = 10;
+function tag(s, v1, v2){
+    console.log(s[0]);  //'Hello'
+    console.log(s[1]);  //'world'
+    console.log(s[2]);  //''
+    console.log(v1);  //15
+    console.log(v2);  //50
+
+    return "OK";
+}
+tag`Hello ${a+b} world ${a*b}`;
+
+
 
