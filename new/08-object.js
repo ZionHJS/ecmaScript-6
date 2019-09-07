e7566e7bad934072a8aca8437cc0c0d32a9a7023
zionHuang <hjszion@gmail.com>
2019-09-07 00:39:53 -0700

    updated Object-assign()

--- new/08-object.js
+++ new/08-object.js
@@ -102,4 +102,65 @@ let obj = {
 obj[key1].name   //"[description]"
 obj[key2].name  //""
 
-//Object.is() 
+//Object.is() 用来比较两个值是否严格相等 与严格比较运算符(===)的行为基本一直
+Object.is('foo','foo');   //true
+Object.is({}, {});  //false
+//不同之处只有两个 +0不等于-0  NaN等于自身
++0 === -0 //true
+NaN === NaN //false
+
+Object.is(+0, -0)   //false
+Object.is(NaN, NaN)   //true
+
+// Object.assign()
+// Object.assign() 方法用于对象的合并 将源对象(source)的所有可枚举属性 复制到目标对象(target)
+const target = { a:1 };
+const source1 = { b:2 };
+const source2 = { c:3 };
+
+Object.assign(target, source1, source2);
+target //{a:1, b:2, c:3}
+//Object.assign方法的第一个参数是目标对象 后面的参数都是源对象
+//如果目标对象与源对象有同名属性 或多个源对象有同名属性 则后面的属性会覆盖前面的属性
+const target = { a:1, b:1};
+const source1 = { b:2, c:2};
+const source2 = { c:3};
+
+Object.assign(target, source1, source2);
+target //{a:1, b:2, c:3}
+
+//如果参数不是对象 则会先转问对象 然后返回
+typeof Object.assign(2)  //"object"
+
+//由于undefined 和 null 无法转换为对象 所以如果它们作为参数 就会报错
+Object.assign(undefined)  //TypeError: Cannot convert undefined or null to object
+Object.assign(null)  //TypeError: Cannot convert undefined or null to object
+//如果非对象参数出现在源对象的位置 那么处理规则有所不同 首先这些参数都会转成对象 如果无法转换 则跳过 这意味着 undefined和null 不在首参数 就不会报错
+let obj = {a:1};
+Object.assign(obj, undefined) === obj   //true
+Object.assign(obj, null) === obj  //true
+
+//其他类型的值 数值，字符串和布尔值 不在首参数 也不会报错。但是，除了字符串会以数组的形式 拷贝入目标对象 其他值都不会产生效果
+const v1 = 'abc';
+const v2 = true;
+const v3 = 10;
+
+const obj = Object.assign({}, v1 ,v2 ,v3);  //v2 和 v3不会产生效果 因为它们不是字符串
+console.log(obj);  //{'0':'a', '1':'b', '2':'c'}
+
+Object('abc'); // => {0: "a", 1: "b", 2: "c", length: 3, [[PrimitiveValue]]: "abc"}  //原因在此
+
+//Object.assign拷贝的属性是有限制的 只拷贝源对象的自身属性,也不拷贝不可枚举的属性
+Object.assign({b:'c'}, Object.defineProperty({}, 'invisible',{
+    enumerable:false,
+    value:'hello'
+}))
+//{b:'c'}  //因为enumerable:false
+
+
+
+
+
+
+
+
