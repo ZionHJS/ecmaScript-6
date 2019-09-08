da968a4c583bd365c8866919d07392076db14347
zionHuang <hjszion@gmail.com>
2019-09-07 21:33:59 -0700

    updated class-extend

--- new/10-class-inherit.js
+++ new/10-class-inherit.js
@@ -246,5 +246,118 @@ var obj = {
 obj.toString();  //MyObject:[object, Object]   //对象总是继承至其他对象
 
 //类的prototype属性和__proto__属性
+//浏览器中 每一个对象都有__proto__属性 指向对应的构造函数prototype属性
+//class 同时具有prototype和__proto__属性 因此有两条同时存在的继承链
+
+//子类的__proto__属性 表示构造函数的继承 总是指向父类
+//子类prototype属性的__proto__属性 表示方法的继承 总是指向父类的prototype属性
+class A{}
+class B extends A {}
+B.__proto__  === A //true
+B.prototype.__proto__ === A.prototype   //true
+
+//继承是按照下面的的模式实现的
+class A{}
+class B{}
+//B的实例继承A的实例
+Object.setPrototypeOf(B.prototype, A.prototype);
+
+//B继承A的静态属性
+Object.setPrototypeOf(B, A)
+
+const b = new B();
+//这里解释下Object.setPrototypeOf
+Object.setPrototypeOf = function(obj, proto){
+    obj.__proto__ = proto;
+    return obj;
+}
+//所以这里实际发生的情况是
+Object.setPrototypeOf(B.prototype, A.prototype);
+//等同于
+B.prototype.__proto__ = A.prototype;
+
+Object.setPrototypeOf(B, A);
+//等同于
+B.__proto__ = A;
+
+//这两条继承链 可以这样理解: 作为一个对象 子类B的原型(__proto__)是父类A  作为一个构造函数 子类B的原型对象(prototype属性)是父类的原型对象(prototype属性)的实例
+Object.create(A.prototype)
+//等同于
+B.prototype.__proto__=A.prototype;
+
+//重点来了
+//extends关键字后面可以跟多种类型的值
+class A extends Object{
+}
+A.__proto__ = Object   //true
+A.prototype.__proto__ = Object.prototype   //true
+//这种情况下 其实A就是构造函数Object的复制  A的实例就是Object的实例
+
+//第二种情况 不存在任何继承
+class A{}
+A.__proto__ === Function.prototype   //true
+A.prototype.__proto__ === Object.prototype   //true
+
+//实例的__proto__属性
+//子类实例的__proto__属性的__proto__属性 指向父类实例的__proto__属性 也就是说子类的原型的原型 是父类的原型
+var p1 = new Point(2,3);
+var p2 = new ColorPoint(2,3,'red');
+
+p2.__proto__ === p1.__proto__ //false
+p2.__proto__.__proto__ === p1.__proto__  //true
+//上面代码中 ColorPoint继承了Point 导致前者原型的原型是后者的原型
+
+//因此 通过子类实例的__proto__.__proto__属性 可以修改父类实例的行为
+p2.__proto__.__proto__.printName = function(){
+    console.log('Ha')
+}
+p1.printName()   //'Ha'
+
+//原生构造函数的继承
+//原生构造函数是指语言的内置构造函数 通常用来生成数据解构  ECMAScript大致有下面这些原生构造函数
+//Boolean()  Number()  String()  Array()  Data() Function()  RegExp()  Error()  Object()
+//原生构造函数的this无法绑定 导致拿不到内部属性
+//ES可以新建父类实例对象的this 然后再用子类的构造函数修饰this 使得父类的所有行为都可以继承 那么就可以继承原生的构造函数了
+class MyArray extends Array{
+    constructor(...arg){
+        super(...arg);
+    }
+}
+var arr = new MyArray();
+arr[0] = 12;
+arr.length //11
+
+arr.length = 0;
+arr[0];  //undefined
+//这里定义了一个MyArray类 继承了Array构造函数 因此就可以从MyArray生成数组的实例 
+//这个例子也说明了 extends关键字不仅可以用来继承类 还可以用来继承原生的构造函数 因此可以在原生数据解构的基础上 定义自己的数据结构
+class VersionedArray extends Array{
+    constructor(){
+        super();
+        this.history = [[]];
+    }
+    commit(){
+        this.history.pushState(this.slice());
+    }
+    revert(){
+        this.splice(0, this.length, ...this.history[this.history.length - 1]);
+    }
+}
+var x = new VersionedArray();
+
+x.push(1);
+x.push(2);
+x  //[1,2]
+x.history  //[[]]
+
+x.commit()
+x.history  //[[],[1,2]]
+
+x.push(3)
+x  //[1,2,3]
+
+x.revert();
+x  //[1,2]
+
 
 
